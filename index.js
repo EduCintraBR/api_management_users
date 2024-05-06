6eb1b1000e1e672b2fd564ee658c14917302d625
eduardocintrantt <eduardocintra@bs.nttdata.com>
2024-05-06 20:24:09 -0300

    Concluding the API

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var bodyParser = require('body-parser')
-var express = require("express")
-var app = express()
-var router = require("./routes/routes")
- 
+const bodyParser = require('body-parser')
+const express = require("express")
+const app = express()
+const router = require("./routes/routes")
+const session = require('express-session')
+require('dotenv').config();
+
+// sessions
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1800000 }
+}))
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
